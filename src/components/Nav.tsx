fbc24d8e4fdba2e8067b6cd0f2f73fff43c6ca1a
Edward Tanguay <edward@tanguay.info>
2025-01-01 18:56:15 +0100

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,35 +1,35 @@
-
 import { NavLink } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
-import * as tools from '../tools';
+import { useLocation } from "react-router-dom";
+import * as tools from "../tools";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useState } from "react";
+import React from "react";
 
 const menuItems = [
 	{
-		idCode: 'welcome',
-		title: 'Welcome'
+		idCode: "welcome",
+		title: "Welcome",
 	},
 	{
-		idCode: 'info',
-		title: 'Info'
+		idCode: "info",
+		title: "Info",
 	},
 	{
-		idCode: 'about',
-		title: 'About'
-	}
-]
+		idCode: "about",
+		title: "About",
+	},
+];
 
 export const Nav = () => {
 	const [showMobileMenu, setShowMobileMenu] = useState(false);
 
 	const location = useLocation();
-	const pageIdCode = tools.chopLeft(location.pathname, '/');
-	const currentMenuItem = menuItems.find(m => m.idCode === pageIdCode);
+	const pageIdCode = tools.chopLeft(location.pathname, "/");
+	const currentMenuItem = menuItems.find((m) => m.idCode === pageIdCode);
 
 	const handleMenuToggle = () => {
-		setShowMobileMenu(!showMobileMenu)
-	}
+		setShowMobileMenu(!showMobileMenu);
+	};
 
 	return (
 		<>
@@ -37,22 +37,40 @@ export const Nav = () => {
 				<nav>
 					<div className="md:hidden bg-slate-500 text-[1.2rem] px-4 py-2 content">
 						<div className="flex justify-between">
-							<p><NavLink to={currentMenuItem.idCode}>{currentMenuItem.title}</NavLink></p>
-							<p className="mt-1 cursor-pointer" onClick={handleMenuToggle}><GiHamburgerMenu /></p>
+							<p>
+								<NavLink to={currentMenuItem.idCode}>
+									{currentMenuItem.title}
+								</NavLink>
+							</p>
+							<p
+								className="mt-1 cursor-pointer"
+								onClick={handleMenuToggle}
+							>
+								<GiHamburgerMenu />
+							</p>
 						</div>
 						{showMobileMenu && (
 							<div>
 								{menuItems.map((menuItem, index) => {
 									return (
-										<>
-											{(menuItem.idCode !== currentMenuItem.idCode && (
-												<div key={index}
-												className="mt-[.2rem]"	
-												><NavLink to={menuItem.idCode}
-													onClick={() => setShowMobileMenu(false)}>{menuItem.title}</NavLink></div>
-											))}
-										</>
-									)
+										<React.Fragment key={index}>
+											{menuItem.idCode !==
+												currentMenuItem.idCode && (
+												<div className="mt-[.2rem]">
+													<NavLink
+														to={menuItem.idCode}
+														onClick={() =>
+															setShowMobileMenu(
+																false
+															)
+														}
+													>
+														{menuItem.title}
+													</NavLink>
+												</div>
+											)}
+										</React.Fragment>
+									);
 								})}
 							</div>
 						)}
@@ -61,12 +79,16 @@ export const Nav = () => {
 						<ul className="flex gap-4">
 							{menuItems.map((menuItem, index) => {
 								return (
-									<>
-										{(
-											<li key={index}><NavLink to={menuItem.idCode}>{menuItem.title}</NavLink></li>
-										)}
-									</>
-								)
+									<React.Fragment key={index}>
+										{
+											<li key={index}>
+												<NavLink to={menuItem.idCode}>
+													{menuItem.title}
+												</NavLink>
+											</li>
+										}
+									</React.Fragment>
+								);
 							})}
 						</ul>
 					</div>
